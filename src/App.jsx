fd3409d7df19dbc8f359d0d7968e8a6fed5a86f9
Malay Srivastava <wealthymalay28@gmail.com>
2025-08-16 12:36:27 +0530

    add code

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from 'react-router-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+
+import Events from './components/Events/Events.jsx';
+import EventDetails from './components/Events/EventDetails.jsx';
+import NewEvent from './components/Events/NewEvent.jsx';
+import EditEvent, {
+  loader as editEventLoader,
+  action as editEventAction,
+} from './components/Events/EditEvent.jsx';
+import { queryClient } from './util/http.js';
+import EditEventOptimistic from './components/Events/EditEventOptimistic.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/events" />,
+  },
+  {
+    path: '/events',
+    element: <Events />,
+    children: [
+      {
+        path: '/events/new',
+        element: <NewEvent />,
+      },
+    ],
+  },
+  {
+    path: '/events/:id',
+    element: <EventDetails />,
+    children: [
+      {
+        path: '/events/:id/edit',
+        element: <EditEvent />,
+        loader: editEventLoader,
+        action: editEventAction
+      },
+    ],
+    /*
+    children: [
+      {
+        path: '/events/:id/edit',
+        element: <EditEventOptimistic />
+      }
+    ]
+    */
+  },
+]);
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
+}
+
+export default App;
